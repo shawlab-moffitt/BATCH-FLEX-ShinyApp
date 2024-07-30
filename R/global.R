ed123bbbf0d9b9efc486a37f3f0bc19fc6caa153
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <aobermayer4@gmail.com>
2024-07-30 11:32:20 -0400

    upload app files
    Uploading app helper files and image files

---
+++ R/global.R
@@ -0,0 +1,50 @@
+
+options(shiny.maxRequestSize = 10000*1024^2)
+
+# Cran
+library(shiny)
+library(shinyjqui)
+library(limma)
+library(bapred)
+library(DT)
+library(shinycssloaders)
+library(metafolio)
+library(corrplot)
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(zip)
+library(glue)
+library(factoextra)
+library(FactoMineR)
+library(magick)
+library(ggfortify)
+library(ggplotify)
+library(draw)
+library(clValid)
+library(shinyWidgets)
+library(tidyr)
+library(umap)
+library(plotly)
+library(statVisual)
+library(svglite)
+library(tabula)
+library(folio)
+library(bslib)
+library(readtext)
+library(shinythemes)
+library(uuid)
+library(stringr)
+# BioConductor
+library(sva)
+library(preprocessCore)#
+library(scater)#
+library(affyPLM)
+library(Harman)
+library(RUVSeq)
+library(GSVA)#
+library(ComplexHeatmap)#
+library(pvca)#
+library(edgeR)#
+# Github
+library(immunedeconv)
